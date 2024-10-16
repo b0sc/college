3f05615006842dbd6450f75b25ed5976132b7c36
leyuskckiran1510 <kdhakal1510@gmail.com>
2024-10-16 21:10:50 +0545

    feat: add reqrite of RTS { main.py => main.cpp }

--- oop/real_time_system/main.cpp
+++ oop/real_time_system/main.cpp
@@ -1,1 +1,40 @@
-//code here
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Sensor {
+public:
+    string name;
+    Sensor(const string& name) : name(name) {}
+
+    // Simulate reading data from a sensor
+    int readData() {
+        return 42;
+    }
+
+};
+
+class RealTimeSystem {
+public:
+    vector<Sensor> sensors;
+    RealTimeSystem() {
+        sensors.push_back(Sensor("Temperature"));
+        sensors.push_back(Sensor("Pressure"));
+    }
+
+    void processData() {
+        for (auto& sensor : sensors) {
+            int data = sensor.readData();
+            cout << sensor.name << ": " << data << endl;
+        }
+    }
+
+};
+
+int main() {
+    RealTimeSystem realTimeSystem;
+    realTimeSystem.processData();
+    return 0;
+}
